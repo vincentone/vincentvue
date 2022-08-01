ed035e4ff88c951f1acfe680f5d6a4896ad3e2a1
vincent <1343327598@qq.com>
2022-08-01 10:41:37 +0800

    项目提交

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        hello it's me!
+        <router-link :to="{name:'AboutMeChilrenA'}">A</router-link>
+        <router-link :to="{name:'AboutMeChilrenB'}">B</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutMe',
+  created () {
+    console.log('I get router params: id :' + this.$route.params.id)
+  }
+}
+</script>
